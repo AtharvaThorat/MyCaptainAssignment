db9eec6b6d9754688de0129c084a03ab5aa3490d
AtharvaThorat <101087445+AtharvaThorat@users.noreply.github.com>
2022-03-15 11:10:32 +0530

    Add files via upload

---
+++ march_batch(circle.area).py
@@ -0,0 +1,7 @@
+#First we import pi from math
+from math import pi
+R = float(input ("Input the radius of the circle : "))
+#Formulating Area
+Area = pi*R**2
+#In order the get the value of input Radius, str(R) is introduced 
+print ("The area of the circle with radius " , str(R),  "is: " ,Area)
